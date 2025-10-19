0a47550338304b951537736eea640cb82646f3fb
syntaxhacker <jrohit072@gmail.com>
2025-10-19 10:27:40 +0530

    remove binance

--- config.py
+++ config.py
@@ -29,23 +29,6 @@ TRADING_CONFIG = {
     }
 }
 
-# Binance API Constants
-BINANCE_API_CONFIG = {
-    'testnet': {
-        'futures_api': 'https://testnet.binancefuture.com',
-        'websocket_base': 'wss://stream.binancefuture.com',
-        'websocket_stream': 'wss://stream.binancefuture.com/ws',
-        'api_key': 'c5079bf884cc676c7d3e799e080df1463b6de7cecf2c9d4b34c376c60a99c491',
-        'api_secret': 'c9c4822f5944527b9ddd79f97fff3d0c33da5814c202020b938150acc4211a44'
-
-    },
-    'mainnet': {
-        'futures_api': 'https://fapi.binance.com',
-        'websocket_base': 'wss://fstream.binance.com',
-        'websocket_stream': 'wss://fstream.binance.com/ws'
-    }
-}
-
 # WebSocket Configuration
 WEBSOCKET_CONFIG = {
     'ping_interval': 20,  # Reduced for more stable connection
